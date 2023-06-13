f39c7e6fa363959dcc1e607276b5323df58c2678
Mbici <lawrencemgitau@gmail.com>
2023-06-13 17:57:44 +0300

    Added print statements for logging

--- simulate.py
+++ simulate.py
@@ -4,6 +4,8 @@ import numpy as np
 
 data = geopandas.read_file(r"C:\Users\mbici\Desktop\Portfolio Ptojects\Spatial peoject\data\hotosm_ken_roads_lines.shp")
 
+print("Loading data complete. Simulating...")
+
 def simulate_data(n = len(data["geometry"])):
     data_dict ={
         "accidents":np.random.randint(100,size=n),
@@ -14,5 +16,11 @@ def simulate_data(n = len(data["geometry"])):
     df = pd.DataFrame(data_dict,columns=["accidents","amounts"])
     simulated_data = data[["name","geometry","highway","oneway"]].join(df)
     return simulated_data
+
 simulated_data = simulate_data()
+
+print("Finished simulating data, Writing to csv")
+
 simulated_data.to_csv("roads.csv")
+
+print("All operations complete")
